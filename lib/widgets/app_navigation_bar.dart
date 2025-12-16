896859259cbfaf09c1b4634a12431c6ee0417fcd
spalt_er <mail@spalt3r.de>
2025-12-16 15:01:45 +0100

    Added a dummy menu for the search box.

--- lib/widgets/app_navigation_bar.dart
+++ lib/widgets/app_navigation_bar.dart
@@ -2,13 +2,112 @@ import 'dart:io' show exit;
 
 import 'package:flutter/material.dart';
 
-class AppNavigationBar extends StatelessWidget implements PreferredSizeWidget {
+class AppNavigationBar extends StatefulWidget implements PreferredSizeWidget {
   final TextEditingController textController;
 
   const AppNavigationBar({
     super.key,
     required this.textController,
   });
+
+  @override
+  State<AppNavigationBar> createState() => _AppNavigationBarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _AppNavigationBarState extends State<AppNavigationBar> {
+  final FocusNode _focusNode = FocusNode();
+  final LayerLink _layerLink = LayerLink();
+  final GlobalKey _textFieldKey = GlobalKey();
+  OverlayEntry? _overlayEntry;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(_onFocusChange);
+  }
+
+  @override
+  void dispose() {
+    _focusNode.removeListener(_onFocusChange);
+    _focusNode.dispose();
+    _removeOverlay();
+    super.dispose();
+  }
+
+  void _onFocusChange() {
+    if (_focusNode.hasFocus) {
+      _showOverlay();
+    } else {
+      _removeOverlay();
+    }
+  }
+
+  void _showOverlay() {
+    _removeOverlay();
+    _overlayEntry = _createOverlayEntry();
+    Overlay.of(context).insert(_overlayEntry!);
+  }
+
+  void _removeOverlay() {
+    _overlayEntry?.remove();
+    _overlayEntry = null;
+  }
+
+  OverlayEntry _createOverlayEntry() {
+    final RenderBox? renderBox = _textFieldKey.currentContext?.findRenderObject() as RenderBox?;
+    final width = renderBox?.size.width ?? 400;
+    
+    return OverlayEntry(
+      builder: (context) => Positioned(
+        width: width,
+        child: CompositedTransformFollower(
+          link: _layerLink,
+          showWhenUnlinked: false,
+          offset: const Offset(0, 50),
+          child: Material(
+            elevation: 4,
+            borderRadius: BorderRadius.circular(8),
+            child: Container(
+              constraints: const BoxConstraints(maxHeight: 300),
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.surface,
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(
+                  color: Theme.of(context).colorScheme.primary.withAlpha(128),
+                ),
+              ),
+              child: ListView(
+                padding: EdgeInsets.zero,
+                shrinkWrap: true,
+                children: [
+                  _buildSuggestionItem('Dummy Item 1', Icons.text_fields),
+                  _buildSuggestionItem('Dummy Item 2', Icons.translate),
+                  _buildSuggestionItem('Dummy Item 3', Icons.book),
+                  _buildSuggestionItem('Dummy Item 4', Icons.school),
+                  _buildSuggestionItem('Dummy Item 5', Icons.language),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSuggestionItem(String text, IconData icon) {
+    return ListTile(
+      dense: true,
+      leading: Icon(icon, size: 20),
+      title: Text(text),
+      onTap: () {
+        widget.textController.text = text;
+        _focusNode.unfocus();
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,32 +125,37 @@ class AppNavigationBar extends StatelessWidget implements PreferredSizeWidget {
           const Spacer(),
           Expanded(
             flex: 3,
-            child: TextField(
-              controller: textController,
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
+            child: CompositedTransformTarget(
+              key: _textFieldKey,
+              link: _layerLink,
+              child: TextField(
+                controller: widget.textController,
+                focusNode: _focusNode,
+                textAlign: TextAlign.left,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.primary.withAlpha(128)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide:
+                        BorderSide(color: Theme.of(context).colorScheme.primary, width: 2),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 4,
+                  ),
+                  isDense: true,
+                  filled: true,
+                  fillColor: Theme.of(context).colorScheme.surface,
                 ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide(
-                      color: Theme.of(context).colorScheme.primary.withAlpha(128)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide:
-                      BorderSide(color: Theme.of(context).colorScheme.primary, width: 2),
-                ),
-                contentPadding: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                  vertical: 4,
-                ),
-                isDense: true,
-                filled: true,
-                fillColor: Theme.of(context).colorScheme.surface,
               ),
             ),
           ),
@@ -69,7 +173,4 @@ class AppNavigationBar extends StatelessWidget implements PreferredSizeWidget {
       ],
     );
   }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
