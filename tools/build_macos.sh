9d614d5a89101d11426f365414f4d2795583ceea
spalt_er <mail@spalt3r.de>
2026-02-05 08:26:24 +0100

    Removed release copy tasks for macos.

--- tools/build_macos.sh
+++ tools/build_macos.sh
@@ -3,8 +3,5 @@
 # Build the Flutter macOS app in release mode
 flutter build macos --release
 
-# Copy the built app to Applications folder
-cp -r build/macos/Build/Products/Release/jpn_learning_diary.app "/Applications/JPN Learning Diary.app"
-
 echo "Build complete and app deployed to Applications folder"
 
